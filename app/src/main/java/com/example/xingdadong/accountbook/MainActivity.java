2cd44170a4065eb36afb495ef324af45d9f50c0d
dxingda <xdong15@syr.edu>
2015-04-20 03:29:04 -0400

    Dxingda Sunday

--- app/src/main/java/com/example/xingdadong/accountbook/MainActivity.java
+++ app/src/main/java/com/example/xingdadong/accountbook/MainActivity.java
@@ -4,57 +4,26 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
 
+public class MainActivity extends ActionBarActivity {
+    public static Data data=new Data();
+    public final static String filename = "data";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        data.readFromFile(filename,this);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
     }
-
-
-/*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        if (id == R.id.action_fancynob) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, Fragment_FancyRotaryKnob.newInstance(0))
-                    .addToBackStack("fancyRotaryKnob")
-                    .commit();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-*/
     /**
      * A placeholder fragment containing a simple view.
      */
@@ -67,7 +36,6 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
             Button b1 = (Button)rootView.findViewById(R.id.button_knob);
             b1.setOnClickListener(new View.OnClickListener() {
                 @Override
