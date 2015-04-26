9e662e6aa44b5d8bc13c5a4fd77d0b0cee7e5132
CUSEJing <jingzhihe2011@gmail.com>
2015-04-26 06:31:56 -0400

    Jing

--- app/src/main/java/com/example/xingdadong/accountbook/Fragment_Therometer.java
+++ app/src/main/java/com/example/xingdadong/accountbook/Fragment_Therometer.java
@@ -1,5 +1,7 @@
 package com.example.xingdadong.accountbook;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
